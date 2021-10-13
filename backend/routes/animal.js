24221266f96d5abd45d361a77c4942267c8841f3
Karolina Obcowska <jimmy.obcowska@gmail.com>
2021-10-13 18:39:21 +0200

    auth middleware created

--- backend/routes/animal.js
+++ backend/routes/animal.js
@@ -1,17 +1,18 @@
 import { Router } from 'express'
 import { createAnimal, getAllAnimals, getAnimalById, deleteAnimal, updateAnimal } from '../controller/animal.js'
+import { isAuth } from '../middleware/isAuth.js'
 
 const router = Router();
 
 router
     .route('/')
-    .get(getAllAnimals)
+    .get(isAuth, getAllAnimals)
     .post(createAnimal)
 
 router
     .route('/:id')
-    .get(getAnimalById)
-    .delete(deleteAnimal)
-    .put(updateAnimal)
+    .get(isAuth, getAnimalById)
+    .delete(isAuth, deleteAnimal)
+    .put(isAuth, updateAnimal)
 
 export default router
