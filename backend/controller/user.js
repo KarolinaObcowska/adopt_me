470d0effad3eb2e271516000fcb19b1c1d5a82a9
Karolina Obcowska <jimmy.obcowska@gmail.com>
2021-10-13 16:39:55 +0200

    login function created

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -22,3 +22,37 @@ export async function signup (req, res, next) {
           next(err);
     }
 }
+
+export async function login (req, res, next) {
+    const { email, password } = req.body
+    try {
+        let user = await User.findOne({ email: email });
+        if (!user) {
+            const error = new Error('User with this e-mail could not be found')
+            error.statusCode = 401
+            throw error
+        }
+        const checkPassword = await bcrypt.compare(password, user.password)
+        if (checkPassword) {
+            const error = new Error('Invalid data!')
+            error.statusCode = 401
+            throw error
+        }
+        const jwtToken = jwt.sign(
+            {
+                email: user.email,
+                userId: user._id.toString()
+            },
+            'secretkey',
+            {
+                expiresIn: '3h'
+            }
+        )
+        res.status(200).json({ msg: 'Logged', token: jwtToken, userId: user._id.toString()})
+    } catch (error) {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    }
+}
