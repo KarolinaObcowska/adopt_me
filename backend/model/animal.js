d0043df59af1c8ca61e094e6c12fba0947e40122
Karolina Obcowska <jimmy.obcowska@gmail.com>
2021-10-13 16:31:08 +0200

    user and animal models added

---
+++ backend/model/animal.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose'
+
+const animalSchema = new mongoose.Schema(
+    {
+        type: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        name: {
+            type: String,
+            trim: true,
+            required: true
+        },
+        breed: {
+            type: String,
+            trim: true,
+            required: true
+        },
+        age: {
+            type: Number,
+            required: true,
+            trim: true
+        }
+    }
+)
+
+export const Animal = mongoose.model("Animal", animalSchema)
---
+++ backend/model/user.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema(
+    {
+        firstname: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        lastname: {
+            type: String,
+            required: true,
+            trmi: true
+        },
+        email: {
+            type: String,
+            required: true,
+            trim: true,
+            unique: true
+        },
+        password: {
+            type: String,
+            required: true,
+            minlength: 8
+        }
+    }
+)
+
+export const User = mongoose.model("User", userSchema)
