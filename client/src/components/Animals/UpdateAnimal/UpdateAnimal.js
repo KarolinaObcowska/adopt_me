6305412ec4411f6ad73968f8888c28026642ac30
Karolina Obcowska <jimmy.obcowska@gmail.com>
2022-01-26 10:14:11 +0100

    split  updateAnimal into two components

--- client/src/components/Animals/UpdateAnimal/UpdateAnimal.js
+++ client/src/components/Animals/UpdateAnimal/UpdateAnimal.js
@@ -2,12 +2,14 @@ import AnimalForm from "../AnimalForm";
 import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import AnimalGallery from "../AnimalGallery/AnimalGallery";
+import UpdateImages from "./UpdateImages";
 
 const UpdateAnimal = () => {
+
   const { id } = useParams();
   const navigate = useNavigate();
+
   const [item, setItem] = useState({});
-  const [images, setImages] = useState([]);
   const [updateAnimal, setUpdateAnimal] = useState({
     type: "",
     breed: "",
@@ -23,7 +25,7 @@ const UpdateAnimal = () => {
       setItem(data.animal);
     }
     fetchAnimalById();
-  }, [item]);
+  }, [item, id]);
 
   function handleChangeInput(event) {
     const { name, value } = event.target;
@@ -33,28 +35,6 @@ const UpdateAnimal = () => {
     });
   }
 
-  function handleImageChange(event) {
-    const files = [...images];
-    files.push(...event.target.files);
-    setImages(files);
-  }
-
-  async function handleUploadImages(event) {
-    event.preventDefault();
-    const formData = new FormData();
-    for (let i = 0; i < images.length; i++) {
-      formData.append("images", images[i]);
-    }
-    const res = await fetch(`/animal/${id}/images`, {
-      method: "POST",
-      body: formData,
-    });
-    if (res.status === 200) {
-      navigate(`/animals/${id}`);
-    } else {
-      console.log("fail");
-    }
-  }
  
   async function handleUpdateAnimal(event) {
     event.preventDefault();
@@ -109,37 +89,7 @@ const UpdateAnimal = () => {
           <span className="text-center inline ml-6 text-5xl font-bold text-green-700">
             {item.name}
           </span>
-          <form
-            className="w-full h-full max-w-lg lg:mb-20 mb-5"
-            onSubmit={handleUploadImages}
-            encType="multipart/form-data"
-          >
-            <div className="flex flex-wrap -mx-3 mb-1">
-              <div className="w-full px-3">
-                <label
-                  className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                  htmlFor="describe"
-                >
-                  Images (max. 10)
-                </label>
-                <input
-                  className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none  focus:border-yellow-500"
-                  type="file"
-                  name="images"
-                  aria-label="upload animal's images"
-                  onChange={handleImageChange}
-                  placeholder="Choose Images"
-                  multiple
-                />
-              </div>
-            </div>
-            <button
-              type="submit"
-              className="bg-yellow-400 p-2 w-full rounded-md text-white uppercase"
-            >
-              Add Images
-            </button>
-          </form>
+          <UpdateImages navigate={navigate} id={id} />
         </div>
         <AnimalForm
           buttonText="Update"
